1c305f0782fdba910f85fdff28b9db34f15f1362
ManiuX <hy530k@163.com>
2019-01-23 10:27:14 +0800

    init project

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture `Getting Started`
+    .page `https://github.com/`;
+
+test('github homepage', async t => {
+    // Test code
+});
---
+++ tests/testdemo.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture `Getting Started`
+    .page `https://reactjs.org/`;
+
+test('react homepage', async t => {
+    // Test code
+});
